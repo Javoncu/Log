316b3520e07ac0a4a824cadb76db7880bd98551b
THINK <THINK@THINK-LX>
2017-09-27 17:35:06 +0800

    添加项目文件。

---
+++ WebA/WebA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebA.Startup))]
+namespace WebA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
